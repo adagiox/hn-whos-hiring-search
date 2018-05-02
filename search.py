7f309302d0259c1d42cc5fac0230ca90f3dfbcba
alea <alea@aleas-iMac.local>
2018-05-01 23:40:58 -0700

    Found the API, but its slow now...

--- search.py
+++ search.py
@@ -1,25 +1,22 @@
 import urllib.request
+import requests
+import json
+import sys
 from fuzzywuzzy import fuzz
 from bs4 import BeautifulSoup
 
-# @TODO():  Better scoring algorithm, i.e. multiple matches increase score
-# @TODO():  Sort output by most matches/best score
-# @TODO():  User input of search terms
-
 search_dict = {
     '1': ['San Francisco', 'Python', 'Intern'],
-    '2': ['Seattle', 'Python', 'Intern'],
-    '3': ['Blockchain'],
-    '4': ['SQL']
+    '2': ['Seattle', 'Python', 'Intern']
 }
 
+results = []
 
 def score_all(comment_text, search_all_dict):
     score_dict = {}
     for search_key, search_list in search_all_dict.items():
         score_dict[search_key] = score_from_search_list(comment_text, search_list)
     return score_dict
-
 
 def score_from_search_list(comment_text, search_list):
     score = 0.0
@@ -29,24 +26,41 @@ def score_from_search_list(comment_text, search_list):
             score += (search_score / len(search_list))
     return score
 
+def get_latest_hiring_post():
+    url = 'https://hacker-news.firebaseio.com/v0/user/whoishiring.json'
+    r = requests.get(url)
+    posts = json.loads(r.text)
+    base_post = 'https://hacker-news.firebaseio.com/v0/item'
+    for post in posts['submitted']:
+        check = requests.get(f'{base_post}/{post}.json')
+        check = json.loads(check.text)
+        if 'Who is hiring?' in check['title']:
+            return post
 
 def search_comments(search_query):
-    url = 'https://news.ycombinator.com/item?id=16492994'
-    page = urllib.request.urlopen(url)
-    soup = BeautifulSoup(page, 'html.parser')
-    for comment in soup.find_all('span', attrs={'class': 'c00'}):
-        comment_text_stripped = comment.get_text(' ')
-        comment_text_stripped = comment_text_stripped.encode('utf-8')
-        comment_text_stripped = comment_text_stripped.decode('latin-1')
-        scored_dict = score_all(comment_text_stripped, search_query)
-        p = ""
-        for key, value in scored_dict.items():
-            if scored_dict[key] > 0:
+    base_items = 'https://hacker-news.firebaseio.com/v0/item'
+    whoishiring = get_latest_hiring_post()
+    comments = requests.get(f'{base_items}/{whoishiring}.json')
+    for comment in json.loads(comments.text)['kids']:
+        comment_id = comment
+        comment = requests.get(f'{base_items}/{comment}.json')
+        comment = json.loads(comment.text)
+        if not comment is None and not 'deleted' in comment:
+            comment = comment['text']
+            comment_text = BeautifulSoup(comment, 'html.parser').get_text(' ')
+            scored_dict = score_all(comment_text, search_query)
+            p = ""
+            total_score = 0
+            for key, value in scored_dict.items():
+                total_score += scored_dict[key]
                 p += f"{scored_dict[key]:<.{4}}\t {search_dict[key]}\n"
-        if p:
-            print(f"{comment_text_stripped}{p}===============\n")
-
+            if total_score > 0:
+                res = f"{p}\n\n{comment_text}\n==============="
+                results.append((total_score, res, comment_id))
 
 if __name__ == '__main__':
     search_comments(search_dict)
-
+    results.sort(key=lambda tup: tup[0], reverse=True)
+    with open('out.txt', 'w+') as f:
+        for r in results:
+            f.write(f'https://news.ycombinator.com/item?id={r[2]}' + '\n' + 'Score: ' + str(r[0]) + '\n' + r[1] + '\n')
